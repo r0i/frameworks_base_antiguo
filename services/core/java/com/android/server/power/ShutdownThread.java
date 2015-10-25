2ceb3982c0c48d7d6f5161b98655d3b87da81274
Alex Cruz <mazdarider23@gmail.com>
2015-10-25 00:15:28 +0000

    Turn advanced reboot on by default [1/2]
    Change-Id: Iac9a74bc70f16a2d191589fa55542441fa1f5312

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -281,8 +281,10 @@ public final class ShutdownThread extends Thread {
     }
 
     private static int getAdvancedReboot(Context context) {
-        return SlimSettings.Secure.getInt(context.getContentResolver(),
-                SlimSettings.Secure.ADVANCED_REBOOT, 0);
+
+          return Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT, 1);
+
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
