98bf601ab2b0882bd6dcdd681b2d35a045a9f4c3
R0i <r0i@darksideteam.com>
2016-07-10 09:42:12 +0200

    Revert "Fix Quick settings tiles font themes derp"
    This reverts commit e0f4e608940e1a0a4ce2e2365934bf23f0ab8630.

--- packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
@@ -45,6 +45,8 @@ import java.util.Objects;
 
 /** View that represents a standard quick settings tile. **/
 public class QSTileView extends ViewGroup {
+    private static final Typeface CONDENSED = Typeface.create("sans-serif-condensed",
+            Typeface.NORMAL);
 
     protected final Context mContext;
     private final View mIcon;
@@ -141,7 +143,7 @@ public class QSTileView extends ViewGroup {
             mDualLabel.setFirstLineCaret(mContext.getDrawable(R.drawable.qs_dual_tile_caret));
             mDualLabel.setTextColor(mContext.getColor(R.color.qs_tile_text));
             mDualLabel.setPadding(0, mDualTileVerticalPaddingPx, 0, mDualTileVerticalPaddingPx);
-            mDualLabel.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
+            mDualLabel.setTypeface(CONDENSED);
             mDualLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
             mDualLabel.setClickable(true);
@@ -161,7 +163,7 @@ public class QSTileView extends ViewGroup {
             mLabel.setGravity(Gravity.CENTER_HORIZONTAL);
             mLabel.setMinLines(2);
             mLabel.setPadding(0, 0, 0, 0);
-            mLabel.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
+            mLabel.setTypeface(CONDENSED);
             mLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
             mLabel.setClickable(false);
